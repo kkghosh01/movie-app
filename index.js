a9fa6c5252437ad5dd87e8ce3e2bf1786f9e8ad4
kkghosh01 <kkghosh0177@gmail.com>
2025-11-04 13:45:45 +0600

    handle all unhandle routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const qs = require("qs");
 const moviesRouter = require("./Routes/moviesRoutes.js");
 const globalErrorHandler = require("./Middlewares/globalErrorHandler.js");
+const AppError = require("./Utils/appError.js");
 
 const app = express();
 
@@ -14,6 +15,9 @@ app.use(express.json());
 // mount movies router
 app.use("/api/v1/movies", moviesRouter);
 
+app.use((req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
+});
 app.use(globalErrorHandler);
 
 module.exports = app;
