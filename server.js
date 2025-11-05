0770217bb3acaf27fed441e0662ad10a159d984c
kkghosh01 <kkghosh0177@gmail.com>
2025-11-05 14:31:53 +0600

    handle unhandledRejection & uncaughtException error

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 const config = require("./config.js");
+
+// Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.error(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require("./index.js");
 
 const uri = `mongodb+srv://${config.dbUser}:${encodeURIComponent(
@@ -9,12 +17,21 @@ const uri = `mongodb+srv://${config.dbUser}:${encodeURIComponent(
 mongoose
   .connect(uri)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
 
 const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port} [${config.env}]`);
+});
 
-app.listen(port, () => {
-  console.log(
-    `Server is running and listening on port ${port} [${config.env}]`
-  );
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.error(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
